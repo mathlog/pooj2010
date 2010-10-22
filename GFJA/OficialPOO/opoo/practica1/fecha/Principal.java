ca6e24c1633741f75b2cb6b5de02f55a9ace9f2a
joseangel.jstyl8 <joseangel.jstyl8@47d606eb-4599-2790-aa7a-a83e3b8abb92>
2010-10-22 07:40:56 +0000

    Modificao nombre practicas

---
+++ GFJA/OficialPOO/opoo/practica1/fecha/Principal.java
@@ -0,0 +1,38 @@
+package opoo.practica1.fecha;
+
+import opoo.excepciones.FechaMalFormadaException;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Fecha f, f2;
+		try {
+			f = new Fecha(16, 12, 1990);
+			f2 = new Fecha(20, 2, 2005);
+			System.out.println(f.extenso());
+			System.out.println(f.mini());
+			System.out.println(f.miniUSA());
+			System.out.println(f.estiloIngles());
+			if (f.compareTo(f2) < 0)
+				System.out.println(f + " es antes que " + f2);
+			else
+				System.out.println(f + " es despues que " + f2);
+			System.out.println("Diferencias tiempo entre " + f + " y " + f2
+					+ ":");
+			
+			System.out.println("Años: " + f.diferenciaYears(f2));
+			System.out.println("Meses: " + f.diferenciaMeses(f2));
+			System.out.println("Semanas: " + f.diferenciaSemanas(f2));
+			System.out.println("Dias: " + f.diferenciaDias(f2));
+			System.out.println("Horas: " + f.diferenciaHoras(f2));
+			System.out.println("Minutos: " + f.diferenciaMinutos(f2));
+			System.out.println("Segundos: " + f.diferenciaSegundos(f2));
+			System.out.println("Completo: " + f.diferenciaCompleto(f2));
+			
+			System.out.println(f+" +20 dias: " + f.sumaDias(20));
+		} catch (FechaMalFormadaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ GFJA/OficialPOO/opoo/practica1/punto/Puntopol.java
@@ -0,0 +1,38 @@
+package opoo.practica1.punto;
+
+class Puntopol {
+	private double angulo = 0;
+	private double modulo = 0;
+
+	public Puntopol(double angulo, double modulo) {
+		this.angulo = angulo;
+		this.modulo = modulo;
+	}
+
+	public Puntopol() {
+		angulo = 0;
+		modulo = 0;
+	}
+
+	public void rotar(double rotacion) {
+		angulo += rotacion;
+
+	}
+
+	public double x() {
+		return modulo * Math.cos(angulo);
+	}
+
+	public double y() {
+		return modulo * Math.sin(angulo);
+	}
+
+	public double angulo() {
+		return angulo;
+	}
+
+	public double modulo() {
+		return modulo;
+	}
+
+}
